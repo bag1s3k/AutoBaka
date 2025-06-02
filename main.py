0cd1a4bf0449a39cc92ae661cf8da60a2d16592b
maty <maty.idk@emai.cz>
2025-06-02 23:25:15 +0200

    feat: function login, data from user using console

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 options = Options()
 options.add_experimental_option("detach", True)
@@ -10,3 +11,10 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                           options=options)
 
 driver.get("https://bakaweb.cichnovabrno.cz/login")
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+driver.find_element(By.NAME, "username").send_keys(username)
+driver.find_element(By.NAME, "password").send_keys(password)
+driver.find_element(By.NAME, "login").click()
