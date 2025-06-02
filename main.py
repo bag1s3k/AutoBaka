56b58cc5fc8d95621d686644589b8a7e23cf5e24
maty <maty.idk@emai.cz>
2025-06-02 23:37:48 +0200

    feat: auto login using temp solution, data from .txt (no encrypt)

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
 
 driver.get("https://bakaweb.cichnovabrno.cz/login")
 
-username = input("Enter username: ")
-password = input("Enter password: ")
+with open("data.txt", "r", encoding="utf-8-sig") as file:
+    username = file.readline().strip()
+    password = file.readline().strip()
+
+print(username)
+print(password)
 
 driver.find_element(By.NAME, "username").send_keys(username)
 driver.find_element(By.NAME, "password").send_keys(password)
