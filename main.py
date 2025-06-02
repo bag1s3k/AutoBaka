49c8f26d3310076d39919b965814ac5628b096b5
maty <maty.idk@emai.cz>
2025-06-03 00:28:59 +0200

    feat: currently on chronological list of marks

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import time
+from bs4 import BeautifulSoup
 
 options = Options()
 options.add_experimental_option("detach", True)
@@ -16,9 +18,20 @@ with open("data.txt", "r", encoding="utf-8-sig") as file:
     username = file.readline().strip()
     password = file.readline().strip()
 
-print(username)
-print(password)
-
+# Logging
 driver.find_element(By.NAME, "username").send_keys(username)
 driver.find_element(By.NAME, "password").send_keys(password)
 driver.find_element(By.NAME, "login").click()
+
+# Navigation to marks
+time.sleep(3)
+driver.find_element(By.CLASS_NAME, "ico32-modul-klasifikace").click()
+time.sleep(1)
+driver.find_element(By.CLASS_NAME, "ico32-modul-klasifikacePrubezna").click()
+time.sleep(2)
+driver.find_element(By.ID, "cphmain_linkchronologicky").click()
+time.sleep(1)
+
+# table = driver.find_element("xpath", "//tbody[.//tr[.//td]]")
+#
+# print(table.get_attribute("innerHTML"))
