70d0b2042b9810a3be0bfa10af3877ad2f337dec
maty <maty.idk@emai.cz>
2025-06-19 00:21:44 +0200

    add config of logging

---
+++ config/logging_conf.py
@@ -0,0 +1,6 @@
+﻿import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(levelname)-8s %(asctime)s - %(name)s - %(message)s",
+                    datefmt="%d.%m.%Y %H:%M:%S"
+                    )
