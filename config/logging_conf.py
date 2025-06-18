aafd7d69a0fedfe86034741ccdcce68597ae9132
maty <maty.idk@emai.cz>
2025-06-19 00:40:12 +0200

    add log file

--- config/logging_conf.py
+++ config/logging_conf.py
@@ -1,6 +1,12 @@
 ﻿import logging
 
-logging.basicConfig(level=logging.DEBUG,
-                    format="%(levelname)-8s %(asctime)s - %(name)s - %(message)s",
-                    datefmt="%d.%m.%Y %H:%M:%S"
-                    )
+def setup_logging():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(levelname)-8s %(asctime)s - %(name)s - %(message)s",
+        datefmt="%d.%m.%Y %H:%M:%S",
+        handlers=[
+            logging.FileHandler("project_log.log"), 
+            logging.StreamHandler()
+        ]
+    )
