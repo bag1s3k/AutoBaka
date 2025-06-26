98d8b87f317a003be06ce7378019bfd3373c368c
maty <maty.idk@emai.cz>
2025-06-26 20:02:12 +0200

    add logs to get_marks (process_marks func)

--- core/marks_processor.py
+++ core/marks_processor.py
@@ -7,7 +7,7 @@ from config.logging_conf import setup_logging
 setup_logging()
 logger = logging.getLogger(__name__)
 
-def get_marks(driver):
+def get_marks(driver) -> dict:
     """
     Extraction marks from baka page
     Args:
@@ -46,6 +46,8 @@ def get_marks(driver):
             date = subject[6].text
             subject_name = unidecode(subject[0].text)
 
+            logger.info(f"Extracting: {mark} {topic} {weight} {date} {subject_name}")
+
             subjects.setdefault(subject_name, []).append({
                 "mark": mark,
                 "topic": topic,
@@ -53,7 +55,7 @@ def get_marks(driver):
                 "date": date
             })
 
-            logger.info("Mark extracted")
+            logger.info(f"Extracted: {mark} {topic} {weight} {date} {subject_name}")
 
         return subjects
 
@@ -61,27 +63,55 @@ def get_marks(driver):
         logger.exception(f"Issue during getting marks: {str(e)}")
         return {}
 
-def process_marks(subjects):
+def process_marks(subjects) -> dict:
+    """
+    Processing marks (1- -> 1.5 or N -> don't add)
+    Calculate averages
+    Args:
+        subjects: dict of marks
+    Returns:
+        dict: sorted dict of processed marks
+    """
+
+    if not subjects:
+        logger.warning("No marks to process")
+        return {}
+
+    logger.info(f"Processing marks")
+
+    # 1- -> 1.5 or N -> don't add and Calculate average
     for subject, list_subject in subjects.items():
-        marks = []
-        for dict_mark in list_subject:
-            if "-" in dict_mark["mark"]:
-                text_to_num = [4.5, 3.5, 2.5, 1.5]
-                dict_mark["mark"] = dict_mark["mark"][::-1]
-                dict_mark["mark"] = text_to_num[int(dict_mark["mark"])]
-            elif not dict_mark["mark"].isdigit():
-                continue
+        logger.info(f"Processing subject: {subject}")
+        try:
+            marks = []
+            for dict_mark in list_subject:
+                if "-" in dict_mark["mark"]:
+                    text_to_num = [4.5, 3.5, 2.5, 1.5]
+                    dict_mark["mark"] = dict_mark["mark"][::-1]
+                    dict_mark["mark"] = text_to_num[int(dict_mark["mark"])]
+                elif not dict_mark["mark"].isdigit():
+                    continue
 
-            marks.append([dict_mark["mark"], dict_mark["weight"]])
+                marks.append([dict_mark["mark"], dict_mark["weight"]])
 
-        mark_times_weight = 0
-        weight_sum = 0
+            logger.info(f"Processing completed successfully")
+            logger.info("Calculating average")
+            # Calculate averages
+            mark_times_weight = 0
+            weight_sum = 0
 
-        for mark in marks:
-            mark_times_weight += float(mark[0]) * float(mark[1])
-            weight_sum += float(mark[1])
+            for mark in marks:
+                mark_times_weight += float(mark[0]) * float(mark[1])
+                weight_sum += float(mark[1])
 
-        average = str(mark_times_weight / weight_sum)[:4]
-        subjects[subject].append({"avg": average})
+            average = str(mark_times_weight / weight_sum)[:4]
+            subjects[subject].append({"avg": average})
 
+            logger.info("Calculating completed successfully")
+
+        except Exception as e:
+            logger.exception(f"Something happened during processing marks")
+            return {}
+
+    logger.info("Subject is gonna be sorted and returned")
     return dict(sorted(subjects.items()))
