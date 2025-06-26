971c2ec961285de4f4d1b018430a4ac6974ecc95
maty <maty.idk@emai.cz>
2025-06-26 21:23:16 +0200

    export.py add logs

--- utils/export.py
+++ utils/export.py
@@ -1,10 +1,29 @@
-﻿from config.logging_conf import setup_logging
+﻿import logging
+
+from config.logging_conf import setup_logging
 
 setup_logging()
+logger = logging.getLogger(__name__)
 
 def export_results(subjects, path):
-    with open(path, "w") as file:
-        for subject, marks in subjects.items():
-            file.write(f"{subject:30} {marks[-1]["avg"]}\n")
+    """
+    Export averages to file
+    Args:
+        subjects: dict of marks
+        path: path to output file
+    Returns:
+        bool
+    """
+    logger.info("Marks are gonna be write down")
 
-    return True
+    if not subjects:
+        logger.warning("Nothing to write")
+    try:
+        with open(path, "w") as file:
+            for subject, marks in subjects.items():
+                file.write(f"{subject:30} {marks[-1]["avg"]}\n")
+        logger.info("Marks were successfully exported")
+        return True
+    except Exception as e:
+        logger.exception(f"Issue during exporting: {str(e)}")
+        return False
