e1c58fccee9f22e745671ddab40104e761bba602
maty <maty.idk@emai.cz>
2025-06-28 17:07:01 +0200

    add find root folder func

--- internal/utils/paths.py
+++ internal/utils/paths.py
@@ -1,6 +1,32 @@
 ﻿from pathlib import Path
+import logging
 
-PROJECT_ROOT = Path(__file__).parent
+from logging_setup import setup_logging
+
+# Set up logging
+setup_logging()
+logger = logging.getLogger(__name__)
+
+def find_project_root(target_folder: str = "autobaka") -> Path:
+    """
+    This func finds project root folder
+    Returns:
+        - absolute path of the project root
+    """
+    path = Path(__file__).resolve() # get current absolut path of this file
+    logger.info("Get current absolut path of this file")
+
+    logger.info("")
+    while path.name != target_folder:
+        if path.parent == path: # protection against infinite loop
+            logger.error(f"Root folder: {target_folder} not found")
+            return Path()
+        path = path.parent
+
+    logger.info(f"Root folder: {path} successfully found")
+    return path
+
+PROJECT_ROOT = find_project_root()
 
 CONFIG_PATH = PROJECT_ROOT / "config.ini"
 LOG_PATH = PROJECT_ROOT / "project_log.log"
