00077931b714f479a48264078a2106fb8f4a8eae
maty <maty.idk@emai.cz>
2025-07-14 14:01:36 +0200

    add new file var_validator, this func validate var and makes logs

---
+++ internal/utils/var_validator.py
@@ -0,0 +1,25 @@
+﻿import logging
+from internal.utils.logging_setup import setup_logging
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
+def var_message(var: any, var_name: str, level: str = "error", error_message: str = "Missing or invalid", right_message: str = "Correct"):
+    """
+    Function which validates and makes logs
+
+    Args:
+        var (any): checking variable
+        var_name (string): name of the checking variable
+        level (string): invalid logger level
+        error_message (string): message which is printed if the variable is invalid
+        right_message (string): message which is written into .log file
+    """
+
+    formate_message = f"Variable: {var_name!r} | {var} | {type(var)}" # formate output of the variable status
+
+    if not var:
+        log_fn = getattr(logger, level.lower(), logger.error)
+        log_fn(f"{error_message}: {formate_message}")
+    else:
+        logger.info(f"{right_message}: {formate_message}")
