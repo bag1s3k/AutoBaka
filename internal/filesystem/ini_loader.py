0119ffae7955380e8bda7a09e0699e8afb621e45
bag1s3k <jetam.mravenec@gmail.com>
2025-09-17 22:32:53 +0200

    remove help print func

--- internal/filesystem/ini_loader.py
+++ internal/filesystem/ini_loader.py
@@ -25,11 +25,3 @@ class AutoCastConfigParser(configparser.ConfigParser):
 
 config = AutoCastConfigParser()
 config.read(INI_PATH, "utf-8")
-
-def read_config():
-    for section in config.sections():
-        print(f"[{section}]")
-        for k, v in config[section].items():
-            print(f"{k} = {v}")
-
-        print()
