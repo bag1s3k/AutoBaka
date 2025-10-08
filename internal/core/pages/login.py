62a4b3b8d7f30d11b3f78891d71bac05c905bbc8
bag1s3k <jetam.mravenec@gmail.com>
2025-10-08 10:28:37 +0200

    Move subclass to new location

---
+++ internal/core/pages/login.py
@@ -0,0 +1,35 @@
+class Login(BasePage):
+    """
+    Inherits from BasePage
+    Use for login
+    """
+
+    @validate_output(
+        error_msg="Login failed",
+        success_msg="Login successful",
+        level="critical"
+    )
+    def login(self, username, password) -> bool:
+        """
+        Specific login logic
+        :param username: username (string)
+        :param password: password (string)
+        :return: True if successful otherwise False
+        """
+        try:
+            # Find required elements on website (username and password field, login button)
+            username_field = self._find_item(target=(By.NAME, "username"))
+            password_field = self._find_item(target=(By.NAME, "password"))
+            login_button = self._find_item(target=(By.NAME, "login"))
+
+            username_field.clear()
+            username_field.send_keys(username)
+            password_field.clear()
+            password_field.send_keys(password)
+            login_button.click()
+
+            return True
+
+        except Exception as e:
+            logger.exception(e)
+            return False
