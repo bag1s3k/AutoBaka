23275e39dfec20864c1628a3cb2fe894cf57d6e9
bag1s3k <jetam.mravenec@gmail.com>
2025-10-12 13:39:46 +0200

    Split old main into brain and general logic and add necessary logic to get last data

--- main.py
+++ main.py
@@ -1,77 +1,46 @@
+import json
 import logging
-import argparse
-import time
 
+from internal.core.brain import main_process
 from internal.core import Absence, Marks, Timetable, Login
-from internal.filesystem.export import export_json, export_results
-from internal.utils.selenium_setup import setup_driver
+from internal.filesystem.paths_constants import MARKS_OUTPUT, TIMETABLE_OUTPUT, RAW_ABSENCE_OUTPUT
 from internal.utils.logging_setup import setup_logging
-from internal.filesystem.env_utils import load_credentials
-from internal.filesystem.paths_constants import PROJECT_ROOT, RAW_MARKS_OUTPUT, MARKS_OUTPUT, TIMETABLE_OUTPUT, \
-    RAW_ABSENCE_OUTPUT
-from internal.filesystem.ini_loader import config
 
-start_time = time.time() # STOPWATCH start
-
-# Set up logging
 setup_logging()
 logger = logging.getLogger(__name__)
 
-# -------------------------------- BEFORE TO ENTER THE WEBSITE ---------------------------------- #
-if not PROJECT_ROOT.exists():
-    logger.error("Project root folder doesn't exist")
+if not (failure := main_process()):
+   logger.info("Main process ended with no error")
 
-driver = setup_driver()
 
-# Load login details and create main_parser
-main_parser = argparse.ArgumentParser(
-    prog=__name__,
-    description="parses cmd-line args to extract user credentials (username and password)",
-)
+def get_local_json_data(path: str, encode="utf-8"):
+    """Get local data when interaction on website fail
+    :param path: source path
+    :param encode: encoding
+    :return json as a python readable format"""
+    with open(path, "r", encoding=encode) as f:
+        f_data = json.load(f)
+        return f_data
 
-username, password = load_credentials(main_parser)
 
-print(".", end="", flush=True) # progress print
+for scraper in failure:
+    # ----- LOGIN ----- #
+    if scraper is Login:
+        logger.error("Loading last local data")
+        marks = get_local_json_data(MARKS_OUTPUT)
+        timetable = get_local_json_data(TIMETABLE_OUTPUT)
+        absence = get_local_json_data(RAW_ABSENCE_OUTPUT)
+    # ----- MARKS ----- #
+    if scraper is Marks:
+        logger.error("Loading last local marks data")
+        marks = get_local_json_data(MARKS_OUTPUT)
 
-# ------------------------------------- ON WEBSITE -------------------------------------- #
-login = Login(driver=driver)
-login.get(config.get_auto_cast("URLS", "login_url"))
-login.scrape(username, password)
+    # ----- ABSENCE ----- #
+    if scraper is Absence:
+        logger.error("Loading last local absence data")
+        absence = get_local_json_data(RAW_ABSENCE_OUTPUT)
 
-print(".", end="", flush=True)
-
-# ----- MARKS ------ #
-marks_page = Marks(driver=driver)
-marks_page.get(config.get_auto_cast("URLS", "marks_url"))
-marks_page.scrape()
-export_json(marks_page.subjects, RAW_MARKS_OUTPUT)
-marks_page.process_marks()
-export_json(marks_page.subjects, MARKS_OUTPUT)
-export_results(marks_page.subjects, config.get_auto_cast("PATHS", "result_path"))
-
-print(".", end="", flush=True)
-
-# ----- TIMETABLE ------ #
-timetable = Timetable(driver=driver)
-timetable.get(config.get_auto_cast("URLS", "timetable_url"))
-timetable.scrape()
-export_json(timetable.timetable, TIMETABLE_OUTPUT)
-
-print(".", end="", flush=True)
-
-# ----- ABSENCE ------ #
-absence = Absence(driver=driver)
-absence.get(config.get_auto_cast("URLS", "absence_url"))
-absence.scrape()
-export_json(absence.absence, RAW_ABSENCE_OUTPUT)
-
-print(".", end="", flush=True)
-
-# --------------------------------------- TERMINATE WEBDRIVER ----------------------------- #
-if config.get_auto_cast("SETTINGS", "quit_driver") is not False:  # let window open or close it
-    driver.quit()
-    logger.info("driver was successfully quit")
-
-print(". Successfully", flush=True) # CLI PRINT
-
-print(f"{round(time.time() - start_time, 5)}s")
+    # ----- TIMETABLE ----- #
+    if scraper is Timetable:
+        logger.error("Loading last local timetable data")
+        timetable = get_local_json_data(TIMETABLE_OUTPUT)
